05d7d19133290b4a0ad7b09f225096e4f12e4e23
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-11-23 11:24:12 +0000

    hello world

---
+++ database-prefixer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name:     Database Prefixer
+ * Plugin URI:      https://sematico.com
+ * Description:     DB Prefix utility.
+ * Author:          Alessandro Tesoro
+ * Author URI:      https://sematico.com
+ * Text Domain:     database-prefixer
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * Database Prefixer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Database Prefixer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Database Prefixer. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package database-prefixer
+ * @author Sematico LTD
+ */
+
+namespace Pressmodo\DB;
+
+if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
+	require dirname( __FILE__ ) . '/vendor/autoload.php';
+}
+
+// File here just for testing purposes.
